2eba87a89fcabb01bcb8771d48f49653a7fdff8a
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-25 12:06:07 +0330

    contact profile page style added

--- src/components/Contact Profile/ContactProfile.jsx
+++ src/components/Contact Profile/ContactProfile.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 const ContactProfile = () => {
   return (
-    <div className="container mx-auto h-full">
-      <div className="flex">
-        <div className="w-1/3">
-          <div className="bg-gray-200 rounded-full h-40 w-40 mx-auto mb-4"></div>
+    
+      <div className="flex h-4/5 mt-16 ml-36 bg-slate-200 w-4/5 rounded-lg">
+        <div className="w-2/5 mt-14">
+          <div className="bg-gray-300 rounded-full h-52 w-52 mx-auto mb-4"></div>
         </div>
 
-        <div className="w-2/3 ml-4 relative">
-          <button className="absolute top-0 right-0 p-2 rounded-full bg-gray-300">
+        <div className="w-3/5 h-inherit ml-4 relative">
+          <button className="absolute top-0 right-0 p-2 rounded-full m-4">
             ...
           </button>
 
-          <div className="flex flex-col">
+          <div className="h-full flex flex-col items-start space-y-10 mt-16">
             <div className="mb-4">
-              <p className="text-lg font-bold">John Doe</p>
+              <p className="text-3xl font-bold">John Doe</p>
             </div>
             <div className="mb-4">
-              <p className="text-gray-600">Phone: 123-456-7890</p>
+              <p className="text-gray-600 text-lg font-semibold">Phone: 123-456-7890</p>
             </div>
             <div className="mb-4">
-              <p className="text-gray-600">Email: johndoe@example.com</p>
+              <p className="text-gray-600 text-lg font-semibold">Email: johndoe@example.com</p>
             </div>
             <div className="mb-4">
-              <p className="text-gray-600">
+              <p className="text-gray-600 text-lg font-semibold">
                 Address: 123 Main St, City, Country
               </p>
             </div>
 
-            <div className="flex justify-between">
+            <div className="w-full flex items-center justify-start space-x-8 ">
               <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
                 Edit
               </button>
@@ -40,7 +40,7 @@ const ContactProfile = () => {
           </div>
         </div>
       </div>
-    </div>
+   
   );
 };
 
